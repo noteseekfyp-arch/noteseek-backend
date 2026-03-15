933812c779fb614daceaeff83219834759a25245
Ali Chauhan <alichauhan36346@gmail.com>
2026-03-15 12:37:37 +0500

    initial commit

--- backend/app/notes/models.py
+++ backend/app/notes/models.py
@@ -18,3 +18,4 @@ class Note(Base):
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
+#intial commit
